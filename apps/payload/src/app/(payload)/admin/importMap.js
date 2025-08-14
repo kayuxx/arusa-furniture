effa8fafa9ed028e682ba53e5d8f8e498a41c377
Zoheir Benghalem <kayuxx@tutanota.com>
2025-08-14 19:48:32 +0100

    import map

--- apps/payload/src/app/(payload)/admin/importMap.js
+++ apps/payload/src/app/(payload)/admin/importMap.js
@@ -1,6 +1,7 @@
 import { SlugComponent as SlugComponent_ed614a00e054dca983517496cfd67245 } from '@repo/payload/fields/slug/SlugComponent'
 import { default as default_e74ad88c8ca275375021c57244942f84 } from '@repo/payload/components/SelectField/SelectCurrency'
 import { default as default_73a247f5a52957fc426c227d04390b2d } from '@repo/payload/components/SelectField/SelectProductAvailability'
+import { default as default_3f1f8970ff525a3c5a9527b724a7c31b } from '@repo/payload/components/SelectField/SelectMarkets'
 import { OverviewComponent as OverviewComponent_a8a977ebc872c5d5ea7ee689724c0860 } from '@payloadcms/plugin-seo/client'
 import { MetaTitleComponent as MetaTitleComponent_a8a977ebc872c5d5ea7ee689724c0860 } from '@payloadcms/plugin-seo/client'
 import { MetaImageComponent as MetaImageComponent_a8a977ebc872c5d5ea7ee689724c0860 } from '@payloadcms/plugin-seo/client'
@@ -15,6 +16,7 @@ export const importMap = {
   "@repo/payload/fields/slug/SlugComponent#SlugComponent": SlugComponent_ed614a00e054dca983517496cfd67245,
   "@repo/payload/components/SelectField/SelectCurrency#default": default_e74ad88c8ca275375021c57244942f84,
   "@repo/payload/components/SelectField/SelectProductAvailability#default": default_73a247f5a52957fc426c227d04390b2d,
+  "@repo/payload/components/SelectField/SelectMarkets#default": default_3f1f8970ff525a3c5a9527b724a7c31b,
   "@payloadcms/plugin-seo/client#OverviewComponent": OverviewComponent_a8a977ebc872c5d5ea7ee689724c0860,
   "@payloadcms/plugin-seo/client#MetaTitleComponent": MetaTitleComponent_a8a977ebc872c5d5ea7ee689724c0860,
   "@payloadcms/plugin-seo/client#MetaImageComponent": MetaImageComponent_a8a977ebc872c5d5ea7ee689724c0860,
